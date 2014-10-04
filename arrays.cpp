b6f45cc9471dd7b88209c4f3d4064cf5cce0da9c
Karnovan Maxim <karnovanm@mail.ru>
2014-10-04 11:06:24 +0400

    Необходимо инициализировать хотя бы 1й элемент массива.

--- arrays.cpp
+++ arrays.cpp
@@ -24,19 +24,19 @@ void task_2()
     //                                 explain your choice in commit msg
 }
 
-#if 0
 // returns zero
 int task_3_zero()
 {
     // Task 3
     // Hint 1: fix with initialization (not assignment)
     // Hint 2: you can use only one zero and not five of them! cf. task 2
-    int a[5];
+    int a[5] = {0};
     
     assert(!a[4]);
     return a[4]; // don't touch this!
 }
 
+#if 0
 void task_4()
 {
     // Task 4: asserts at the end must hold
@@ -85,4 +85,5 @@ void task_7_print(int * arr, int size, char delim = ' ')
 int main()
 {
 	task_2();
+	assert(task_3_zero() == 0);
 }
